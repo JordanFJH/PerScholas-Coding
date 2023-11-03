454ca8389e4859d355b1a4d84468ade47c9d1e92
Jordan <higgsfj@gmail.com>
2023-11-03 13:36:23 -0400

    HW

---
+++ DOM-Manipulation-HW/script.js
@@ -0,0 +1,76 @@
+let quotes = [
+  `I live my life a quarter mile at a time`,
+  `I said a ten-second car, not a ten-minute car`,
+  `You can have any brew you want... as long as it's a Corona.`,
+  `You almost had me? You never had me - you never had your car!`,
+  `I don't have friends. I have family.`,
+  `It don't matter if you win by an inch or a mile. Winning's winning.`
+];
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  // Random quote of the day generator
+  const randomQuote = function() {
+    document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
+  };
+  randomQuote();
+  
+  // Do all of your work inside the document.addEventListener  
+
+  // Part 1
+  document.querySelector("#main-title").innerHTML = "This is DOM Toretto's homepage";
+
+  // Part 2
+  //Changing background color of body
+  document.querySelector("body").style.backgroundColor = "Yellow";
+
+  // Part 3
+  document.querySelector("#favorite-things").lastElementChild.remove();
+
+  // Part 4
+  let specialTitles = document.querySelectorAll(".special-title");
+  for (let title of specialTitles){
+    title.style.fontSize = "2rem";
+  }
+
+  // Part 5
+  // console.log(document.querySelector("#past-races").children[3])
+  document.querySelector("#past-races").children[3].remove();
+
+  // Part 6
+  let newRace = document.createElement("li");
+  newRace.innerHTML = "The 313 (Detroit, fyi)";
+  document.querySelector("#past-races").appendChild(newRace);
+
+  // Part 7
+  //Creting an extra blog post
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("blog-post");
+  newDiv.classList.add("purple");
+  let newH2 = document.createElement("h1");
+  newH2.innerHTML = "Detroit";
+  newP = document.createElement("p");
+  newP.innerHTML = "It's called the Motor City for a reason!";
+  newDiv.appendChild(newH2);
+  newDiv.appendChild(newP);
+  document.querySelector(".main").appendChild(newDiv);
+
+  // Part 8
+  document.querySelector("#quote-title").addEventListener("click",function(evt){
+    randomQuote();
+  })
+
+  // Part 9
+  //Toggling class for mousover
+  let blogPosts = document.querySelectorAll(".blog-post");
+  for (let post of blogPosts){
+    post.addEventListener("mouseout", function(evt){
+      post.classList.toggle("purple");
+      post.classList.toggle("red");
+    })
+    post.addEventListener("mouseenter", function(evt){
+      post.classList.toggle("red");
+      post.classList.toggle("purple");
+    })
+  }
+  
+});
